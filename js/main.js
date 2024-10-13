3e09690d39bcd3199eea42a34fc7d6e2b9917cd8
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-13 21:13:16 +0200

    added zombie  class and spawning

--- js/main.js
+++ js/main.js
@@ -39,18 +39,86 @@ class Player {
     }
 }
 
+class Zombies{
+    constructor(){
+        this.width = 2;
+        this.height = 2;
+        this.positionX;
+        this.positionY;
+        this.spawnZombie();
+        this.createDomElements();
+    }
+    createDomElements() {
+        this.domElement = document.createElement("div");
+        this.domElement.classList.add("zombie") ;
+        this.domElement.style.width = this.width + "em";
+        this.domElement.style.height = this.height + "em";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
 
+        const game = document.getElementById("gameBoard");
+        game.appendChild(this.domElement);
+    }
+    spawnZombie(){
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1) );
+        this.positionY = Math.floor(Math.random() * (100 - this.height + 1) );
+    }
+    moveUp() {
+        this.positionY++;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+
+    moveDown() {
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+    moveLeft() {
+        this.positionX--;
+        this.domElement.style.left = this.positionX + "vw";
+
+    }
+    moveRight() {
+        this.positionX++;
+        this.domElement.style.left = this.positionX + "vw";
+    }
+
+    zombieMovment(playerPositionX, playerPosistionY){
+        if(this.positionX > playerPositionX){
+            this.moveLeft();
+        }else if (this.positionX < playerPositionX){
+            this.moveRight();
+        }
+
+
+    }
+}
 
 
 let player = new Player;
+let zombie = [];
+
+let spawn = setInterval(() => {
+    if (zombie.length < 100) {
+        zombie.push(new Zombies)
+    } else {
+        clearInterval(spawn)
+    }
+}, 100);
+
+
+
+
+
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowLeft") {
         player.moveLeft();
     } else if (e.code === "ArrowRight"  ) {
         player.moveRight();
+
     }else if(e.code === "ArrowUp"){
         player.moveUp();
+        
     }else if(e.code === "ArrowDown"){
         player.moveDown();
     }
