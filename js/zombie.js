0317d416b56286357b392daf804e0c00a0c0473b
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-16 11:48:55 +0200

    added zombie spawn logic, implemented spawn free area around player

--- js/zombie.js
+++ js/zombie.js
@@ -1,11 +1,10 @@
 class Zombies {
-    constructor() {
+    constructor(playerX, playerY) {
         this.width = 3;
         this.height = 4.5;
         this.positionX;
         this.positionY;
-        this.spawnZombie();
-        this.createDomElements();
+        this.spawnZombie(playerX, playerY);
     }
     createDomElements() {
         this.domElement = document.createElement("div");
@@ -14,14 +13,30 @@ class Zombies {
         this.domElement.style.height = this.height + "vh";
         this.domElement.style.left = this.positionX + "vw";
         this.domElement.style.bottom = this.positionY + "vh";
-
         const game = document.getElementById("gameBoard");
         game.appendChild(this.domElement);
     }
-    spawnZombie() {
+
+    spawnZombie(playerX, playerY) {
         //spawning at a random location on the gameboard
-        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
-        this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
+        const spawnRadius = 10; //area around player
+
+        do {
+            //keep trying to find a suitable spawn point 
+            let positionX = Math.floor(Math.random() * (100 - this.width + 1));
+            let positionY = Math.floor(Math.random() * (100 - this.height + 1));
+            const distanceX = playerX - positionX;
+            const distanceY = playerY - positionY;
+            const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            if (distance > spawnRadius + this.width / 2) { // if spawn is away from player. spawn zombie
+                this.positionX = positionX;
+                this.positionY = positionY
+                this.createDomElements();
+
+            }
+        } while (this.positionX === undefined)
+
+
     }
     moveUp() {
 
@@ -36,13 +51,13 @@ class Zombies {
     moveLeft() {
         this.positionX--;
         this.domElement.style.left = this.positionX + "vw";
-        
+
 
     }
     moveRight() {
         this.positionX++;
         this.domElement.style.left = this.positionX + "vw";
-        
+
     }
 
 }
